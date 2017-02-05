18a761c21242cea2b2b4a2291c49e04970e34980
Caleb Froese <caleb.froese@gmail.com>
2017-02-05 22:00:16 +1030

    Use a country from a common known one or a lower chance for a random one

--- server/custom/player/generate.js
+++ server/custom/player/generate.js
@@ -1,86 +1,108 @@
 var faker = require('faker');
 
-module.exports = {
-    createPlayer: () => {
+module.exports =
+    {
+      createPlayer: () => {
         return createPlayer();
+      }
     }
+
+function
+createPlayer() {
+  var player = {}
+  // Luck
+  var luck = genLuck();
+  // Name
+  player.first = faker.name.firstName(0);
+  player.last = faker.name.lastName();
+  player.country = genCountry();
+  // Speed / Weight
+  var weightSpeedObj = genWeight(luck);
+  player.spd = weightSpeedObj.spd;
+  player.kg = weightSpeedObj.kg;
+  // Atk / Def
+  var atkDefObj = genAtkDef(luck);
+  player.atk = atkDefObj.atk;
+  player.def = atkDefObj.def;
+  // Recovery rate
+  player.rec = genNumber(0, 30);
+  return player;
 }
 
-function createPlayer() {    
-    var player = { }
-    // Luck
-    var luck = genLuck();
-    // Name
-    player.first = faker.name.firstName(0);
-    player.last = faker.name.lastName();
-    player.country = faker.address.country();
-    // Speed / Weight
-    var weightSpeedObj = genWeight(luck);
-    player.spd = weightSpeedObj.spd;
-    player.kg = weightSpeedObj.kg;
-    // Atk / Def
-    var atkDefObj = genAtkDef(luck);
-    player.atk = atkDefObj.atk;
-    player.def = atkDefObj.def;
-    // Recovery rate
-    player.rec = genNumber(0, 30);
-    return player;
+function
+genCountry() {
+  if (Math.random() > 0.7) {
+    // Use a common known country
+    var countries = [
+      'Australia', 'USA', 'England', 'France', 'USA', 'Spain', 'Japan', 'China',
+      'New Zealand', 'Canada', 'Germany', 'Italy'
+    ];
+    return countries[0, Math.floor(Math.random() * countries.length)]
+  } else {
+    // Use a random country
+    return faker.address.country()
+  }
 }
 
 function genNumber(min, max) {
-    // Generates a random number between variables
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  // Generates a random number between variables
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function genLuck() {
-    // LUCK PERCENT
-    var rand = genNumber(0, 100);
-    var luck;
+function
+genLuck() {
+  // LUCK PERCENT
+  var rand = genNumber(0, 100);
+  var luck;
 
-    if(rand < 2) {
-        // 2%
-        luck = genNumber(0, 30);
-    } else if(rand < 10) {
-        // 8%
-        luck = genNumber(30, 50);
-    } else if(rand < 26) {
-        // 16%
-        luck = genNumber(50, 55);
-    } else if(rand < 5) {
-        // 24%
-        luck = genNumber(55, 65);
-    } else if(rand < 74) {
-        // 24%
-        luck = genNumber(65, 70);
-    } else if(rand < 90) {
-        // 16%
-        luck = genNumber(70, 75);
-    } else if(rand < 98) {
-        // 8%
-        luck = genNumber(75, 85);
-    } else {
-        // 2%
-        luck = genNumber(85, 100);
-    }
-    return luck
+  if (rand < 2) {
+    // 2%
+    luck = genNumber(0, 30);
+  } else if (rand < 10) {
+    // 8%
+    luck = genNumber(30, 50);
+  } else if (rand < 26) {
+    // 16%
+    luck = genNumber(50, 55);
+  } else if (rand < 5) {
+    // 24%
+    luck = genNumber(55, 65);
+  } else if (rand < 74) {
+    // 24%
+    luck = genNumber(65, 70);
+  } else if (rand < 90) {
+    // 16%
+    luck = genNumber(70, 75);
+  } else if (rand < 98) {
+    // 8%
+    luck = genNumber(75, 85);
+  } else {
+    // 2%
+    luck = genNumber(85, 100);
+  }
+  return luck
 }
 
 function genWeight(luck) {
-    var luckElement = genNumber(0, luck) / 4;
+  var luckElement = genNumber(0, luck) / 4;
 
-    var spd = genNumber(50, 100);
-    var kgRatio = 1 - (spd - 50) / 50;
-    var kgLowest = 60 + (kgRatio * genNumber(10, 30));
-    var kgHighest = kgLowest + 10 + (kgRatio * genNumber(30, 100));
-    var kg = genNumber(kgLowest, kgHighest);
-    return { spd: Math.floor(spd + luckElement), kg: Math.floor(kg + luckElement) }
+  var spd = genNumber(50, 100);
+  var kgRatio = 1 - (spd - 50) / 50;
+  var kgLowest = 60 + (kgRatio * genNumber(10, 30));
+  var kgHighest = kgLowest + 10 + (kgRatio * genNumber(30, 100));
+  var kg = genNumber(kgLowest, kgHighest);
+  return {
+    spd: Math.floor(spd + luckElement), kg: Math.floor(kg + luckElement)
+  }
 }
 
 function genAtkDef(luck) {
-    var luckElement = genNumber(0, luck) / 3;
+  var luckElement = genNumber(0, luck) / 3;
 
-    var atk = genNumber(50, 100);
-    var atkRatio = 1 - (atk - 50) / 50;
-    var def = 50 + genNumber(50 * atkRatio, 100 * atkRatio);
-    return { atk: Math.floor(atk + luckElement), def: Math.floor(def + luckElement) }
+  var atk = genNumber(50, 100);
+  var atkRatio = 1 - (atk - 50) / 50;
+  var def = 50 + genNumber(50 * atkRatio, 100 * atkRatio);
+  return {
+    atk: Math.floor(atk + luckElement), def: Math.floor(def + luckElement)
+  }
 }
