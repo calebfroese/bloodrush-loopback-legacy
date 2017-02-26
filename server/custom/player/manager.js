6db352a02c3fffcd500c592a52141cd5f81e2803
Caleb Froese <caleb.froese@gmail.com>
2017-02-26 17:48:16 +1030

    Log state ends

--- server/custom/player/manager.js
+++ server/custom/player/manager.js
@@ -10,42 +10,48 @@ module.exports =
         // Initializes the queue for the first time
         logging.info('Initializing the player manager');
 
-        nodeSchedule.scheduleJob(`playerManager.refreshPlayers`, '0 0 */1 * *', () => {
-          logging.info('Player manager is managing states');
-          findPlayer({where: {state: {inq: ['injured', 'market', 'training']}}})
-              .then(players => {
-                players.forEach(player => {
-                  if (stateEnded(player)) {
-                    // The player is no longer in a temp state
-                    var oldState = player.state;
-                    player.state = 'ok';
-                    player.stateEnds = undefined;
-                    internalQuery(
-                        'patch', `/players/${player.id}`, player, () => {
-                          logging.info(`Player ${player.first} ${player.last
-                                       } is no longer ${oldState}`);
-                        })
-                  }
-                });
-              })
-              .catch(err => {
-                logging.error('Error in player manager:');
-                logging.error(err);
-              })
-        });
+        nodeSchedule.scheduleJob(
+            `playerManager.refreshPlayers`, '0 0 */1 * *', () => {
+              logging.info('Player manager is managing states');
+              findPlayer(
+                  {where: {state: {inq: ['injured', 'market', 'training']}}})
+                  .then(players => {
+                    logging.info(
+                        'Player manager fetched ' + players.length +
+                        ' players with stateEnds');
+                    players.forEach(player => {
+                      if (stateEnded(player)) {
+                        // The player is no longer in a temp state
+                        var oldState = player.state;
+                        player.state = 'ok';
+                        player.stateEnds = undefined;
+                        internalQuery(
+                            'patch', `/players/${player.id}`, player, () => {
+                              logging.info(`Player ${player.first} ${player.last
+                                           } is no longer ${oldState}`);
+                            })
+                      }
+                    });
+                  })
+                  .catch(err => {
+                    logging.error('Error in player manager:');
+                    logging.error(err);
+                  })
+            });
       }
     }
 
 function findPlayer(query) {
   return new Promise((resolve, reject) => {
     internalQuery(
-        'get', `/players?filter={"where": {"state": {"inq": ["injured", "market", "training"]}}}`, {},
-        players => {resolve(players)});
+        'get',
+        `/players?filter={"where": {"state": {"inq": ["injured", "market", "training"]}}}`,
+        {}, players => {resolve(players)});
   });
 }
 
 function stateEnded(player) {
   if (!player.stateEnds) return false;
-  if (moment(player.stateEnds).isBefore(moment())) return true;
+  if (moment().isAfter(player.stateEnds)) return true;
   return false;
 }
